e5f593dce27548195b9fd1cc9fb42539b22917e9
mrofiqul <rmd.rofiqulislam@gmail.com>
2014-07-21 13:29:17 +0600

    21/07/2014 Circle

---
+++ Md.Rofiqul Islam/C#/ConsoleApp/CircleApplication/CircleApplication/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApplication
+{
+    class Circle
+    {
+        public double radius;
+        private const double PI = 3.1416;
+       
+
+        public double Area(double radius)
+        {
+
+            return PI * radius * radius;
+
+        }
+
+        public double Diameter(double radius)
+        {
+        
+            return 2 * radius;
+
+        }
+
+        public double Perimeter(double radius)
+        {
+    
+            return 2 * PI * radius;
+        }
+    }
+}
---
+++ Md.Rofiqul Islam/C#/ConsoleApp/CircleApplication/CircleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApplication
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+           
+
+            Console.WriteLine("enter the value of radius:");
+            
+         aCircle.radius =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the value of Area:"+  aCircle.Area(aCircle.radius));
+            Console.WriteLine("enter the value of Area:" + aCircle.Diameter(aCircle.radius));
+            Console.WriteLine("enter the value of Area:" + aCircle.Perimeter(aCircle.radius));
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
